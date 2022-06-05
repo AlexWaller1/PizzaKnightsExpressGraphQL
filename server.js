c2363317e27b1a32a6ec30830f6e0146fbfefb8f
Alex Waller <alex@artofalexwaller.com>
2022-06-05 19:48:07 -0400

    graphicql

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
 const express = require("express");
+const expressGraphQL = require("express-graphql");
+const app = express();
+
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true
+  })
+);
+app.listen(3007, () => console.log("Server Running on 3007"));
