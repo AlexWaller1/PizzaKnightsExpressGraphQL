cd7c175929caa4904efd2f7e847c3e85b2e13bef
Alex Waller <alex@artofalexwaller.com>
2022-06-05 21:10:01 -0400

    setting up schema

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express");
 
 const { graphqlHTTP } = require("express-graphql");
-const { GraphQLSchema, GraphQLObjectType } = require("graphql");
+const { GraphQLSchema, GraphQLObjectType, GraphQLString } = require("graphql");
 const app = express();
+
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Hello World",
+    fields: () => ({
+      message: {
+        type: GraphQLString,
+        resolve: () => "Hello World"
+      }
+    })
+  })
+});
 
 app.use(
   "/graphql",
   graphqlHTTP({
+    schema: schema,
     graphiql: true
   })
 );
